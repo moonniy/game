4cbc1ec5677e48575cc82e707af37f8cce50b334
moonniy <moonniy.2012@gmail.com>
2020-03-24 00:03:55 -0600

    Interacción con el teclado

--- game.js
+++ game.js
@@ -1,8 +1,18 @@
+var gameSettings = {
+    playerSpeed: 200,
+}
+
 var config ={
     width: 256,
     height: 272,
     backgroundColor: 0x000000,
-    scene:[Scene1, Scene2]
+    scene:[Scene1, Scene2],
+    physics:{
+	default: "arcade",
+	arcade: {
+	    debug: false
+	}
+    }
 }
 
 window.onload = function(){
